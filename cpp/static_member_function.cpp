b26668211a114362c12e428697155064209ca294
Riken <riken.prajapati@matrixcomsec.com>
2025-01-08 11:40:02 +0530

    Adding Direct Call of Static Member Function without Object Instance

--- cpp/static_member_function.cpp
+++ cpp/static_member_function.cpp
@@ -28,17 +28,20 @@ int main()
 {
     Counter obj1, obj2, obj3;
 
-    obj1.incrementCount();  // Increment count (count = 1)
-    cout << "obj1: " << obj1.getCount() << endl;  // Output: 1
+    Counter::incrementCount(); // Increment count (count = 1)
+    cout << "Counter::incrementCount: " << obj1.getCount() << endl;  // Output: 1
 
-    obj2.incrementCount();  // Increment count (count = 2)
+    obj1.incrementCount();  // Increment count (count = 2)
+    cout << "obj1: " << obj1.getCount() << endl;  // Output: 2
+
+    obj2.incrementCount();  // Increment count (count = 3)
+    cout << "obj2: " << obj2.getCount() << endl;  // Output: 3
+
+    obj3.decrementCount();  // Decrement count (count = 2)
+    cout << "obj3: " << obj3.getCount() << endl;  // Output: 2
+
+    cout << "obj1: " << obj1.getCount() << endl;  // Output: 2
     cout << "obj2: " << obj2.getCount() << endl;  // Output: 2
-
-    obj3.decrementCount();  // Decrement count (count = 1)
-    cout << "obj3: " << obj3.getCount() << endl;  // Output: 1
-
-    cout << "obj1: " << obj1.getCount() << endl;  // Output: 1
-    cout << "obj2: " << obj2.getCount() << endl;  // Output: 1
 
     return 0;
 }
